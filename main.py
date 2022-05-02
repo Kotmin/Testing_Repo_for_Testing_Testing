f61f4efeaf92c76676f5b83de0889cab0a88e828
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-05-02 18:22:54 +0200

    ind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
-
-class main:# tak wiem niepoprawnie
-    def tstring_comparsion():
-        with open('Kto_ma_co.txt') as f:
-                print(f.read())
+def tstring_comparsion():
+    with open('Kto_ma_co.txt') as f:
+        print(f.read())
 
