c35e9083e3108c360348dfc1eb506b7b5d0def2d
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-05-02 18:10:30 +0200

    New indentations

--- test_main.py
+++ test_main.py
@@ -1,11 +1,9 @@
 import pytest
 
-from main import main
-class TestIsThisOriginalText:
-    def test_tstring_comparsion():
-            with open('Kto_ma_co.txt') as f:
-                to_cmp=f.read()
-            assert to_cmp.strip()=="Ala ma psa od 2022-04-30"
+def test_tstring_comparsion():
+        with open('Kto_ma_co.txt') as f:
+            to_cmp=f.read()
+        assert to_cmp.strip()=="Ala ma psa od 2022-04-30"
             
         
 
